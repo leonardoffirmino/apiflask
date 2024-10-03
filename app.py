1be7ad96dd4ae461df16bc0eab561ae66ddd3eba
leonardoffirmino <firmino750@gmail.com>
2024-10-02 22:27:13 -0300

    Create route delete

--- app.py
+++ app.py
@@ -55,6 +55,20 @@ def update_task(id):
   task.completed = data['completed']
   print(task)
   return jsonify({"message":"Tarefa atualizada com sucesso!"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+  
+  if not task:
+    return jsonify({"message":"Não foi possivel localizar a atividade!"}),404
+  
+  tasks.remove(task)
+  return jsonify({"message": "Tarefa deletada com sucesso!"})
   
 
 if __name__ == "__main__":
