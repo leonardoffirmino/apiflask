d1b3e4cc33e67765475be778491eecc457f5d039
Leonardo <firmino750@gmail.com>
2024-10-05 10:45:34 -0300

    Create file test pytest

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def create_task():
   tasks.append(new_task)
   print(tasks)
 
-  return  jsonify({"message":"Nova tarefa criada com sucesso"})
+  return  jsonify({"message":"Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route('/tasks',methods=['GET'])
 def get_tasks():
