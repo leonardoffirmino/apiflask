8cd6f5d3d47a69ba045b5efb55a4d14aec7dbff9
leonardoffirmino <firmino750@gmail.com>
2024-10-08 22:37:56 -0300

    Add route teste delete and put item

--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 
-#Crud
+#Crud para instalar a versão do pacote devemos rodar o pip3 install -r nomearquivo
 BASE_URL = 'http://127.0.0.1:5000'
 tasks =[]
 
@@ -34,3 +34,34 @@ def test_get_task():
     assert response.status_code == 200
     response_json = response.json()
     assert task_id == response_json['id']
+
+def test_update_task():
+  if tasks:
+    task_id = tasks[0]
+    payload = {
+      "completed":True,
+      "description": "Nova descrição",
+      "title": "Titulo atualizado"
+    }
+    response = requests.put(f"{BASE_URL}/tasks/{task_id}",json=payload)
+    response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+
+  #Nova requisição para a tarefa especifica 
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json["title"] == payload["title"]
+    assert response_json["description"] == payload["description"]
+    assert response_json["completed"] == payload["completed"]
+
+def test_delete_task():
+  if tasks:
+    task_id = tasks[0]
+    response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+    response.status_code == 200
+
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 404
+    
